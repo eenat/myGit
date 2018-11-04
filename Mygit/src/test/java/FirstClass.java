a357b3917b3506b335a461db8d0ac56d8b2e44e0
eenat <etarnovskaya@hotmail.com>
2018-11-04 19:46:51 +0200

    5-th added before class

--- Mygit/src/test/java/FirstClass.java
+++ Mygit/src/test/java/FirstClass.java
@@ -1,3 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+
 public class FirstClass {
+  WebDriver wd;
+  @BeforeClass
+  public void setUp() {
+    wd = new ChromeDriver();
+  }
+
 
 }
